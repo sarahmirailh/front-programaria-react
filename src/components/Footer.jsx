171bdc88b7395995fa3ef3ae2f4ab22007483e01
sarahmirailh <sarahmirailh@id.uff.br>
2023-05-28 20:48:44 -0300

    Complementando o cabeçalho.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/footer.module.css'
 export function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <p className={styles.footerText}>Front-end feito com ♡ por Simara Conceição para PrograMaria.</p>
+      <p className={styles.footerText}>Front-end feito com ♡ por Simara Conceição para PrograMaria. <br/>Back-end feito com ♡ e dedicação pela aluna Sarah Mirailh. <br/>Curso BackEnd #1 - PrograMaria</p>
     </div>
   )
 }
